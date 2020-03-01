9bf6c6d9e167d1f55cdda2477d916f086d9c1ba5
MediocrePhysicist <lboc.campos@gmail.com>
2020-03-01 18:37:15 -0300

    Logout

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import date
 login_page = 'https://primofs01.sibi.usp.br/pds?func=load-login&calling_system=primo&institute=USP&lang=por&isMobile=false&url=http://www.buscaintegrada.usp.br:80/primo_library/libweb/action/login.do?targetURL=http%3a%2f%2fwww.buscaintegrada.usp.br%2fprimo_library%2flibweb%2faction%2fsearch.do%3fvid%3dUSP%26amp%3bdscnt%3d0%26amp%3bdstmp%3d1583081070253%26amp%3binitializeIndex%3dtrue'
 
 browser = webdriver.Firefox()
+browser.implicitly_wait(5)
 browser.get(login_page)
 browser.find_element_by_link_text('Senha da biblioteca').click()
 
@@ -19,7 +20,7 @@ login_user.send_keys(secrets.NUSP)
 login_pass.send_keys(secrets.PASS)
 
 browser.find_element_by_xpath('//*[@value = "OK"]').click()
-browser.find_element_by_link_text('Minha Conta').click()
+browser.find_element_by_id('exlidMyAccount').click()
 
 
 # Book Info
@@ -50,3 +51,8 @@ for i in selected:
     # Return date didn't change after selection => the book must be returned
     if dates[i].text == new_dates[i].text:
         returned.append(i)
+
+
+# Logout
+browser.find_element_by_id('exlidSignOut').click()
+browser.quit()
