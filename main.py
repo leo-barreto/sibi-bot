26c29af89952c8bd2403c10dd0aeab5f6c71de61
MediocrePhysicist <lboc.campos@gmail.com>
2020-03-01 19:03:14 -0300

    Improved output

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
-import secrets
 from datetime import date
+import time
+import secrets
+
+
+start = time.time()
 
 
 # Setup Browser
 login_page = 'https://primofs01.sibi.usp.br/pds?func=load-login&calling_system=primo&institute=USP&lang=por&isMobile=false&url=http://www.buscaintegrada.usp.br:80/primo_library/libweb/action/login.do?targetURL=http%3a%2f%2fwww.buscaintegrada.usp.br%2fprimo_library%2flibweb%2faction%2fsearch.do%3fvid%3dUSP%26amp%3bdscnt%3d0%26amp%3bdstmp%3d1583081070253%26amp%3binitializeIndex%3dtrue'
 
 browser = webdriver.Firefox()
-browser.implicitly_wait(5)
+browser.implicitly_wait(10)
 browser.get(login_page)
 browser.find_element_by_link_text('Senha da biblioteca').click()
 
@@ -31,28 +35,36 @@ locals = browser.find_elements_by_xpath('//*[@class = "MyAccount_Loans_7 MyAccou
 
 checkboxes.pop() # Last element is not associated with a book
 td = date.today().strftime("%d/%m/%y")
+N = len(dates)
 selected = [] # Books that must be renewed
-returned = [] # Books that must be returned
 
-for i in range(len(dates)):
-    print('\nBook {0}: {1}'.format(str(i), titles[i].text))
-
+for i in range(N):
     if dates[i].text == td:
         checkboxes[i].click()
         selected.append(i)
-        print('\t... Selected.')
-    else:
-        print('\t... OK')
 
 browser.find_element_by_link_text('Renovar selecionados').click()
 new_dates = browser.find_elements_by_xpath('//*[@class = "MyAccount_Loans_4 MyAccount_Loans_dueDate"]')
 
-for i in selected:
-    # Return date didn't change after selection => the book must be returned
-    if dates[i].text == new_dates[i].text:
-        returned.append(i)
+
+# Output
+for i in range(N):
+    print('\nBook {0}: "{1}"'.format(str(i), titles[i].text))
+
+    if i in selected:
+        # Return date didn't change after selection => the book must be returned
+        if dates[i].text == new_dates[i].text:
+            print('\t... Must be returned to {}.'.format(locals[i].text))
+        else:
+            print('\t... Successful renewal.')
+
+    else:
+        print('\t... OK.')
 
 
 # Logout
 browser.find_element_by_id('exlidSignOut').click()
 browser.quit()
+
+time_exec = time.time() - start
+print('\n\nDONE! (in {0:.2f} seconds)'.format(time_exec))
